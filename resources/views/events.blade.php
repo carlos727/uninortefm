3e8aa3b07c0430737162fea3a98960d2f63a8f1a
carlos727 <carlos72793@hotmail.com>
2016-04-02 16:55:56 -0500

    Try Solve Errors

--- resources/views/events.blade.php
+++ resources/views/events.blade.php
@@ -42,79 +42,94 @@
 									<div class="col s6">
 										<label for="event-start-at">Hora de Inicio</label>
 										<div id="event-start-at" class="input-field">
-											<i class="material-icons prefix">alarm</i>
-											<select name="start_at_h">
-												<option value="00" selected="selected">00</option>
-												<option value="01">01</option>
-												<option value="02">02</option>
-												<option value="03">03</option>
-												<option value="04">04</option>
-												<option value="05">05</option>
-												<option value="06">06</option>
-												<option value="07">07</option>
-												<option value="08">08</option>
-												<option value="09">09</option>
-												<option value="10">10</option>
-												<option value="11">11</option>
-												<option value="12">12</option>
-												<option value="13">13</option>
-												<option value="14">14</option>
-												<option value="15">15</option>
-												<option value="16">16</option>
-												<option value="17">17</option>
-												<option value="18">18</option>
-												<option value="19">19</option>
-												<option value="20">20</option>
-												<option value="21">21</option>
-												<option value="22">22</option>
-												<option value="23">23</option>
-											</select>
-											<select name="start_at_m">
-												<option value="00" selected="selected">00</option>
-												<option value="15">15</option>
-												<option value="30">30</option>
-												<option value="45">45</option>
-												<option value="59">59</option>
-											</select>
+											<div class="row">
+												<div class="col s1"><i class="material-icons prefix">alarm</i></div>
+												<div class="col s2">
+													<select name="start_at_h">
+														<option value="00" selected="selected">00</option>
+														<option value="01">01</option>
+														<option value="02">02</option>
+														<option value="03">03</option>
+														<option value="04">04</option>
+														<option value="05">05</option>
+														<option value="06">06</option>
+														<option value="07">07</option>
+														<option value="08">08</option>
+														<option value="09">09</option>
+														<option value="10">10</option>
+														<option value="11">11</option>
+														<option value="12">12</option>
+														<option value="13">13</option>
+														<option value="14">14</option>
+														<option value="15">15</option>
+														<option value="16">16</option>
+														<option value="17">17</option>
+														<option value="18">18</option>
+														<option value="19">19</option>
+														<option value="20">20</option>
+														<option value="21">21</option>
+														<option value="22">22</option>
+														<option value="23">23</option>
+													</select>
+												</div>
+												<div class="col s1"><p>:</p></div>
+												<div class="col s2">
+													<select name="start_at_m">
+														<option value="00" selected="selected">00</option>
+														<option value="15">15</option>
+														<option value="30">30</option>
+														<option value="45">45</option>
+														<option value="59">59</option>
+													</select>
+												</div>
+											</div>
 											<input name="start_at" type="time" class="hide" value="">
 										</div>
 									</div>
 									<div class="col s6">
 										<label for="event-end-at">Hora de Finalizacion</label>
 										<div id="event-end-at">
-											<select name="end_at_h">
-												<option value="00" selected="selected">00</option>
-												<option value="01">01</option>
-												<option value="02">02</option>
-												<option value="03">03</option>
-												<option value="04">04</option>
-												<option value="05">05</option>
-												<option value="06">06</option>
-												<option value="07">07</option>
-												<option value="08">08</option>
-												<option value="09">09</option>
-												<option value="10">10</option>
-												<option value="11">11</option>
-												<option value="12">12</option>
-												<option value="13">13</option>
-												<option value="14">14</option>
-												<option value="15">15</option>
-												<option value="16">16</option>
-												<option value="17">17</option>
-												<option value="18">18</option>
-												<option value="19">19</option>
-												<option value="20">20</option>
-												<option value="21">21</option>
-												<option value="22">22</option>
-												<option value="23">23</option>
-											</select>
-											<select name="end_at_m">
-												<option value="00" selected="selected">00</option>
-												<option value="15">15</option>
-												<option value="30">30</option>
-												<option value="45">45</option>
-												<option value="59">59</option>
-											</select>
+											<div class="row">
+												<div class="col s1"><i class="material-icons prefix">alarm</i></div>
+												<div class="col s2 input-field">
+													<select name="end_at_h">
+														<option value="00" selected="selected">00</option>
+														<option value="01">01</option>
+														<option value="02">02</option>
+														<option value="03">03</option>
+														<option value="04">04</option>
+														<option value="05">05</option>
+														<option value="06">06</option>
+														<option value="07">07</option>
+														<option value="08">08</option>
+														<option value="09">09</option>
+														<option value="10">10</option>
+														<option value="11">11</option>
+														<option value="12">12</option>
+														<option value="13">13</option>
+														<option value="14">14</option>
+														<option value="15">15</option>
+														<option value="16">16</option>
+														<option value="17">17</option>
+														<option value="18">18</option>
+														<option value="19">19</option>
+														<option value="20">20</option>
+														<option value="21">21</option>
+														<option value="22">22</option>
+														<option value="23">23</option>
+													</select>
+												</div>
+												<div class="col s1"><p>:</p></div>
+												<div class="col s2 input-field">
+													<select name="end_at_m">
+														<option value="00" selected="selected">00</option>
+														<option value="15">15</option>
+														<option value="30">30</option>
+														<option value="45">45</option>
+														<option value="59">59</option>
+													</select>
+												</div>
+											</div>
 											<input name="end_at" type="time" class="hide" value="">
 										</div>
 									</div>
