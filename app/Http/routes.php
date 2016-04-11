9c71d6b66fb9759b0d7bad73582d5b5e2ed8a7b7
carlos727 <carlos72793@hotmail.com>
2016-04-11 01:43:15 -0500

    Validations

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -58,11 +58,11 @@ Route::group(['middleware' => 'web'], function () {
 			$conts = 0;
 			$conte =  0;
 			foreach ($events as $event) {
-				if ($event->start_at < $start_at && $start_at < $event->end_at) {
+				if (strtotime($event->start_at) < strtotime($start_at) && strtotime($start_at) < strtotime($event->end_at)) {
 					$conts++;
 				}
 
-				if ($event->start_at < $end_at && $end_at < $event->end_at) {
+				if (strtotime($event->start_at) < strtotime($end_at) && strtotime($end_at) < strtotime($event->end_at)) {
 					$conte++;
 				}
 			}
